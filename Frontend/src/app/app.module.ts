901409fe0c2a6ebd549821869dd3371e6d92cae9
TobiasRafael <rafaelmontas96@gmail.com>
2021-02-24 22:49:21 -0400

    Property Added

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { PropertyComponent } from './property/property.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    PropertyComponent
   ],
   imports: [
     BrowserModule
